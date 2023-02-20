d708e7f7afba29cacab7be723451ea244091273f
Nelson Ochieng <ochiengnel@gmail.com>
2023-02-20 12:07:27 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	Char alp[26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	Putchar('n\');
+	return (0);
+}
