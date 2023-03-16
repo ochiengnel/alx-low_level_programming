396c720a039905a6f20ab96c01a2ab0227e030c4
Nelson Ochieng <ochiengnel@gmail.com>
2023-03-16 13:25:51 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,7 +8,7 @@
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
-		
+
 int is_digit(char *s)
 {
 	int i = 0;
@@ -27,15 +27,12 @@ int is_digit(char *s)
 }
 
 /**
-
  * _strlen - returns the length of a string
-
  * @s: string to evaluate
  *
  * Return: the length of the string
-		
  */
-int _strlen(char *s)		
+int _strlen(char *s)
 {
 
 	int i = 0;
@@ -66,9 +63,8 @@ void errors(void)
 
 }
 
-
 /**
- * main - multiplies two positive numbers		
+ * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -80,7 +76,6 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
@@ -92,9 +87,7 @@ int main(int argc, char *argv[])
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
-		
 	{
-
 		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
@@ -104,34 +97,19 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
-
 		if (carry > 0)
-
 			result[len1 + len2 + 1] += carry;
-
 	}
-
 	for (i = 0; i < len - 1; i++)
-
 	{
 		if (result[i])
-
 			a = 1;
-
 		if (a)
-
 			_putchar(result[i] + '0');
-
 	}
-
 	if (!a)
-
 		_putchar('0');
-
 	_putchar('\n');
-
 	free(result);
-
 	return (0);
-
 }
