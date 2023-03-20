e26fb822a0fe6589b3da441197f68a14897d4015
Nelson Ochieng <ochiengnel@gmail.com>
2023-03-20 09:26:54 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *doggy;
 
 	nlen = olen = 0;
-	while (name[nlen++]);
-
-	while (owner[olen++]);
-
+	while
+		(name[nlen++]);
+	while
+		(owner[olen++]);
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
 		return (NULL);
